ebbfd3aba32fda299e071c04498cc4f40257b3d0
Jonas Munck a.R <munck.jonas@gmail.com>
2015-10-24 01:28:59 +0200

    Enable PhantomJs, resolve karma-dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -15,11 +15,9 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
             'karma-jasmine',
             'karma-junit-reporter'
             ],
